c2361dad0a1544785a275d8bcae7e24912199f02
Jeff <jeff@casimircreative.com>
2010-04-28 00:17:51 -0400

    Removing second method definition

--- lib/gcal4ruby/calendar.rb
+++ lib/gcal4ruby/calendar.rb
@@ -382,64 +382,65 @@ class Calendar
     "<iframe src='http://www.google.com/calendar/embed?#{output}' style='#{params[:border]} px solid;' width='#{params[:width]}' height='#{params[:height]}' frameborder='#{params[:border]}' scrolling='no'></iframe>"  
   end
   
-  #Helper function to return a specified calendar id as a formatted iframe embedded google calendar.  This function does not require loading the calendar information from the Google calendar
-  #service, but does require you know the google calendar id. 
-  #1. *id*: the unique google assigned id for the calendar to display.
-  #2. *params*: a hash of parameters that affect the display of the embedded calendar:
-  #   height:: the height of the embedded calendar in pixels
-  #   width:: the width of the embedded calendar in pixels
-  #   title:: the title to display
-  #   bgcolor:: the background color.  Limited choices, see google docs for allowable values.
-  #   color:: the color of the calendar elements.  Limited choices, see google docs for allowable values.
-  #   showTitle:: set to 'false' to hide the title
-  #   showDate:: set to 'false' to hide the current date
-  #   showNav:: set to 'false to hide the navigation tools
-  #   showPrint:: set to 'false' to hide the print icon
-  #   showTabs:: set to 'false' to hide the tabs
-  #   showCalendars:: set to 'false' to hide the calendars selection drop down
-  #   showTimezone:: set to 'false' to hide the timezone selection
-  #   border:: the border width in pixels
-  #   dates:: a range of dates to display in the format of 'yyyymmdd/yyyymmdd'.  Example: 20090820/20091001
-  #   privateKey:: use to display a private calendar.  You can find this key under the calendar settings pane of the Google Calendar website.
-  def self.to_iframe(id, params = {})
-    params[:id] = id
-    params[:height] ||= "600"
-    params[:width] ||= "600"
-    params[:bgcolor] ||= "#FFFFFF"
-    params[:color] ||= "#2952A3"
-    params[:showTitle] = params[:showTitle] == false ? "showTitle=0" : ''
-    params[:showNav] = params[:showNav] == false ? "showNav=0" : ''
-    params[:showDate] = params[:showDate] == false ? "showDate=0" : ''
-    params[:showPrint] = params[:showPrint] == false ? "showPrint=0" : ''
-    params[:showTabs] = params[:showTabs] == false ? "showTabs=0" : ''
-    params[:showCalendars] = params[:showCalendars] == false ? "showCalendars=0" : ''
-    params[:showTimezone] = params[:showTimezone] == false ? 'showTz=0' : ''
-    params[:border] ||= "0"
-    output = ''
-    params.each do |key, value|
-      case key
-        when :height then output += "height=#{value}"
-        when :width then output += "width=#{value}"
-        when :title then output += "title=#{CGI.escape(value)}"
-        when :bgcolor then output += "bgcolor=#{CGI.escape(value)}"
-        when :showTitle then output += value
-        when :showDate then output += value
-        when :showNav then output += value
-        when :showPrint then output += value
-        when :showTabs then output += value
-        when :showCalendars then output += value
-        when :showTimezone then output += value
-        when :viewMode then output += "mode=#{value}"
-        when :dates then output += "dates=#{CGI.escape(value)}"
-        when :privateKey then output += "pvttk=#{value}"
-      end
-      output += "&amp;"
-    end
-  
-    output += "src=#{params[:id]}&amp;color=#{CGI.escape(params[:color])}"
-        
-    "<iframe src='http://www.google.com/calendar/embed?#{output}' style='#{params[:border]} px solid;' width='#{params[:width]}' height='#{params[:height]}' frameborder='#{params[:border]}' scrolling='no'></iframe>"  
-  end
+  # #Helper function to return a specified calendar id as a formatted iframe embedded google calendar.  This function does not require loading the calendar information from the Google calendar
+  # #service, but does require you know the google calendar id. 
+  # #1. *id*: the unique google assigned id for the calendar to display.
+  # #2. *params*: a hash of parameters that affect the display of the embedded calendar:
+  # #   height:: the height of the embedded calendar in pixels
+  # #   width:: the width of the embedded calendar in pixels
+  # #   title:: the title to display
+  # #   bgcolor:: the background color.  Limited choices, see google docs for allowable values.
+  # #   color:: the color of the calendar elements.  Limited choices, see google docs for allowable values.
+  # #   showTitle:: set to 'false' to hide the title
+  # #   showDate:: set to 'false' to hide the current date
+  # #   showNav:: set to 'false to hide the navigation tools
+  # #   showPrint:: set to 'false' to hide the print icon
+  # #   showTabs:: set to 'false' to hide the tabs
+  # #   showCalendars:: set to 'false' to hide the calendars selection drop down
+  # #   showTimezone:: set to 'false' to hide the timezone selection
+  # #   border:: the border width in pixels
+  # #   dates:: a range of dates to display in the format of 'yyyymmdd/yyyymmdd'.  Example: 20090820/20091001
+  # #   privateKey:: use to display a private calendar.  You can find this key under the calendar settings pane of the Google Calendar website.
+  # def self.to_iframe(id, params = {})
+  #   params[:id] = id
+  #   params[:height] ||= "600"
+  #   params[:width] ||= "600"
+  #   params[:bgcolor] ||= "#FFFFFF"
+  #   params[:color] ||= "#2952A3"
+  #   params[:showTitle] = params[:showTitle] == false ? "showTitle=0" : ''
+  #   params[:showNav] = params[:showNav] == false ? "showNav=0" : ''
+  #   params[:showDate] = params[:showDate] == false ? "showDate=0" : ''
+  #   params[:showPrint] = params[:showPrint] == false ? "showPrint=0" : ''
+  #   params[:showTabs] = params[:showTabs] == false ? "showTabs=0" : ''
+  #   params[:showCalendars] = params[:showCalendars] == false ? "showCalendars=0" : ''
+  #   params[:showTimezone] = params[:showTimezone] == false ? 'showTz=0' : ''
+  #   params[:showTitle] ||= "0"
+  #   params[:viewMode] ||= "WEEK"
+  #   params[:border] ||= "0"
+  #   
+  #   params.collect do |key, value|
+  #     case key
+  #       when :height then ["height", value]
+  #       when :width then ["width", value]
+  #       when :title then ["title", CGI.escape(value)]
+  #       #when :bgcolor then output += "bgcolor=#{CGI.escape(value)}"
+  #       when :showTitle then ["showTitle", value]
+  #       #when :showDate then output += value
+  #       #when :showNav then output += value
+  #       #when :showPrint then output += value
+  #       #when :showTabs then output += value
+  #       #when :showCalendars then output += value
+  #       #when :showTimezone then output += value
+  #       when :viewMode then ["mode", value]
+  #       #when :dates then output += "dates=#{CGI.escape(value)}"
+  #       #when :privateKey then output += "pvttk=#{value}"
+  #     end
+  #   end
+  # 
+  #   output += "src=#{params[:id]}&amp;color=#{CGI.escape(params[:color])}"
+  #       
+  #   "<iframe src='http://www.google.com/calendar/embed?#{output}' style='#{params[:border]} px solid;' width='#{params[:width]}' height='#{params[:height]}' frameborder='#{params[:border]}' scrolling='no'></iframe>"  
+  # end
 
   private
   @xml 
